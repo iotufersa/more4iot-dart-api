6802ac51899db4e261ba4dceec36f50745e73759
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2021-06-26 14:59:42 -0300

    initial commit

---
+++ example/more4iot_dart_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:more4iot_dart_api/more4iot_dart_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/more4iot_dart_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library more4iot_dart_api;
+
+export 'src/more4iot_dart_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
